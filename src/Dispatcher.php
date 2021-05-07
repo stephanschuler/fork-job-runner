2ee2f5aab09a2008a54d42f5d7f550b03f2427a0
Stephan Schuler <mail@g4n.de>
2021-05-07 03:53:11 +0200

    Dispatcher now passes workload on to Loop

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -3,23 +3,132 @@ declare(strict_types=1);
 
 namespace StephanSchuler\ForkJobRunner;
 
+use RuntimeException;
+use StephanSchuler\ForkJobRunner\Utility\PackageSerializer;
 use Traversable;
+use function escapeshellcmd;
+use function fclose;
+use function fgets;
+use function file_exists;
+use function fopen;
+use function fputs;
+use function getenv;
+use function is_array;
+use function is_resource;
+use function posix_mkfifo;
+use function proc_close;
+use function proc_get_status;
+use function proc_open;
+use function sys_get_temp_dir;
+use function tempnam;
+use function unlink;
 
 class Dispatcher
 {
+    const RETURN_CHANNEL = 'RETURN_CHANNEL';
+
+    /** @var string */
+    private $loopPath;
+
+    /** @var string */
+    private $returnChannelPath;
+
+    /** @var ?resource */
+    private $loopProcess;
+
+    /** @var ?resource */
+    private $commandChannel;
+
+    public function __construct(string $loopPath)
+    {
+        $this->loopPath = $loopPath;
+
+        $this->returnChannelPath = (string)tempnam(sys_get_temp_dir(), 'return-channel');
+        unlink(@$this->returnChannelPath);
+        posix_mkfifo($this->returnChannelPath, 0600);
+    }
+
+    public function __destruct()
+    {
+        $this->closeLoop();
+    }
+
     /**
      * @param Job $job
      * @return Traversable<string>
      */
     public function run(Job $job): Traversable
     {
-        /** @var string[] $results */
-        $results = [];
+        $this->ensureLoop();
 
-        $writeBack = function (string $line) use (&$results): void {
-            $results[] = $line;
-        };
-        $job->run($writeBack);
-        yield from $results;
+        $blockReturnChannel = fopen($this->returnChannelPath, 'ab+');
+        $returnChannel = fopen($this->returnChannelPath, 'rb');
+        if (!$returnChannel) {
+            throw new RuntimeException('Return channel unavailable');
+        }
+
+        if (!$this->commandChannel) {
+            throw new RuntimeException('Command channel unavailable');
+        }
+        fputs($this->commandChannel, PackageSerializer::toString($job));
+
+        while (!(($subject = fgets($returnChannel)) === false)) {
+            if ($blockReturnChannel) {
+                fclose($blockReturnChannel);
+                $blockReturnChannel = null;
+            }
+
+            yield $subject;
+        }
+    }
+
+    /** @param resource|null $process */
+    private static function isRunnung($process): bool
+    {
+        if (!is_resource($process)) {
+            return false;
+        }
+        $status = proc_get_status($process);
+        if (!is_array($status)) {
+            return false;
+        }
+        if ($status['running'] === true && $status['exitcode'] === -1) {
+            return true;
+        }
+        return false;
+    }
+
+    private function ensureLoop(): void
+    {
+        if (self::isRunnung($this->loopProcess)) {
+            return;
+        }
+
+        $command = escapeshellcmd(PHP_BINARY) . ' ' . escapeshellarg($this->loopPath);
+
+        $descriptors = [
+            ['pipe', 'rb'],
+        ];
+
+        $this->loopProcess = proc_open(
+            $command,
+            $descriptors,
+            $pipes,
+            null,
+            [self::RETURN_CHANNEL => $this->returnChannelPath, 'AUTOLOADER' => getenv('AUTOLOADER')]
+        ) ?: null;
+
+        $this->commandChannel = $pipes[0];
+    }
+
+    private function closeLoop(): void
+    {
+        if ($this->loopProcess) {
+            proc_close($this->loopProcess);
+            $this->loopProcess = null;
+        }
+        if (file_exists($this->returnChannelPath)) {
+            unlink($this->returnChannelPath);
+        }
     }
 }
--- tests/DispatchingTest.php
+++ tests/DispatchingTest.php
@@ -3,31 +3,56 @@ declare(strict_types=1);
 
 namespace StephanSchuler\ForkJobRunner\Tests;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use StephanSchuler\ForkJobRunner\Dispatcher;
-use StephanSchuler\ForkJobRunner\Job;
+use function file_put_contents;
+use function iterator_to_array;
+use function sys_get_temp_dir;
+use function tempnam;
 
 class DispatchingTest extends TestCase
 {
-    /**
-     * @test
-     */
+    /** @test */
     public function Dispatcher_call_jobs_and_return_data(): void
     {
-        $job = new class implements Job {
-            /** @inheritDoc */
-            public function run(callable $writeBack): void
-            {
-                $writeBack('first line');
-                $writeBack('second line');
-            }
-        };
+        $job = new TestJob('first line', 'second line');
 
-        $dispatcher = new Dispatcher();
+        putenv('AUTOLOADER=' . __DIR__ . '/../vendor/autoload.php');
+        $phpcode = <<<'PHP'
+<?PHP
+declare(strict_types=1);
+
+require getenv('AUTOLOADER');
+
+use StephanSchuler\ForkJobRunner\Loop;
+
+$loop = new Loop('php://stdin', getenv('RETURN_CHANNEL'));
+$loop->run();
+PHP;
+        $loop = self::tmpfile('loop');
+        file_put_contents($loop, $phpcode);
+
+        $dispatcher = new Dispatcher($loop);
         $result = $dispatcher->run($job);
 
         self::assertInstanceOf(\Generator::class, $result);
-        self::assertEquals(['first line', 'second line'], iterator_to_array($result));
+        self::assertEquals(
+            ['noop' . PHP_EOL, 'first line' . PHP_EOL, 'second line' . PHP_EOL],
+            iterator_to_array($result)
+        );
     }
 
+    protected static function tmpfile(string $reason): string
+    {
+        $tmpfile = tempnam(sys_get_temp_dir(), $reason);
+
+        if (!$tmpfile) {
+            $message = 'Could not create tmpfile "' . $reason . '"';
+            self::markTestIncomplete($message);
+            throw new Exception($message);
+        }
+
+        return $tmpfile;
+    }
 }
