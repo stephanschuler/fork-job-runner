c15812872031775509877a181e17d56eb559fda1
Stephan Schuler <mail@g4n.de>
2021-05-07 02:35:29 +0200

    Loop now forks before running the job

--- src/Loop.php
+++ src/Loop.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace StephanSchuler\ForkJobRunner;
 
+use RuntimeException;
 use StephanSchuler\ForkJobRunner\Utility\PackageSerializer;
+use Throwable;
 use function assert;
 use function fgets;
+use function pcntl_fork;
+use function pcntl_waitpid;
+use function set_exception_handler;
 use function trim;
-use const PHP_EOL;
 
 class Loop
 {
@@ -26,13 +30,38 @@ class Loop
 
     public function run(): void
     {
-        while ($data = trim((string)fgets($this->commandChannel), PHP_EOL)) {
-            $job = PackageSerializer::fromString($data);
-            assert($job instanceof Job);
-            $writer = function (string $line): void {
-                echo $line . PHP_EOL;
-            };
-            $job->run($writer);
+        while ($data = trim((string)fgets($this->commandChannel), PackageSerializer::SPLITTER)) {
+            $child = pcntl_fork();
+
+            if ($child === -1) {
+                throw new RuntimeException('Could not fork into isolated execution');
+            } elseif ($child === 0) {
+                $this->asChild($data);
+            } else {
+                $this->asParent($child);
+            }
         }
+
+        exit;
+    }
+
+    final protected function asChild(string $data): void
+    {
+        set_exception_handler(static function (Throwable $throwable): void {
+            throw $throwable;
+        });
+
+        $job = PackageSerializer::fromString($data);
+        assert($job instanceof Job);
+        $job->run(function (string $line) {
+            echo $line . PHP_EOL;
+        });
+
+        exit;
+    }
+
+    final protected function asParent(int $child): void
+    {
+        pcntl_waitpid($child, $statuscode);
     }
 }
--- tests/LoopTest.php
+++ tests/LoopTest.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace StephanSchuler\ForkJobRunner\Tests;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use StephanSchuler\ForkJobRunner\Loop;
 use StephanSchuler\ForkJobRunner\Utility\PackageSerializer;
-use function fopen;
-use function fputs;
-use function rewind;
+use function file_put_contents;
+use function proc_open;
+use function sys_get_temp_dir;
+use function tempnam;
 
 class LoopTest extends TestCase
 {
@@ -17,17 +19,60 @@ class LoopTest extends TestCase
      */
     public function Loop_calls_jobs_from_command_stream(): void
     {
+        $phpcode = <<<'PHP'
+<?PHP
+declare(strict_types=1);
+
+require getenv('AUTOLOADER');
+
+use StephanSchuler\ForkJobRunner\Loop;
+
+$loop = new Loop(STDIN);
+$loop->run();
+PHP;
+
+        $tmpfile = self::tmpfile('phpcode');
+        file_put_contents($tmpfile, $phpcode);
+
+        $input = self::tmpfile('input');
+        $output = self::tmpfile('output');
+
         $job = new TestJob('line 1', 'line 2');
+        file_put_contents($input, PackageSerializer::toString($job));
 
-        /** @var resource $stream */
-        $stream = fopen('php://temp', 'w+');
-        fputs($stream, PackageSerializer::toString($job));
+        $proc = proc_open(
+            'php ' . $tmpfile,
+            [['file', $input, 'r'], ['file', $output, 'w'], ['file', $output, 'w'],],
+            $pipes,
+            getcwd() ?: null,
+            ['AUTOLOADER' => __DIR__ . '/../vendor/autoload.php',]
+        );
+        if (!$proc) {
+            self::markTestIncomplete('sub process could not be created');
+            return;
+        }
 
-        rewind($stream);
+        while (true) {
+            $status = proc_get_status($proc);
+            if (is_array($status) && $status['running'] === false) {
+                break;
+            }
+        }
+        proc_close($proc);
 
-        $loop = new Loop($stream);
+        self::assertStringEqualsFile($output, 'line 1' . PHP_EOL . 'line 2' . PHP_EOL);
+    }
 
-        self::expectOutputString('line 1' . PHP_EOL . 'line 2' . PHP_EOL);
-        $loop->run();
+    protected static function tmpfile(string $reason): string
+    {
+        $tmpfile = tempnam(sys_get_temp_dir(), $reason);
+
+        if (!$tmpfile) {
+            $message = 'Could not create tmpfile "' . $reason . '"';
+            self::markTestIncomplete($message);
+            throw new Exception($message);
+        }
+
+        return $tmpfile;
     }
 }
