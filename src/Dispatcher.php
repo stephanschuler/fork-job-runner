bc29a7bdb5619bc37f1aca46e15294c485e1aac9
Stephan Schuler <mail@g4n.de>
2021-05-09 21:34:01 +0200

    Check for running loop process

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -5,9 +5,9 @@ namespace StephanSchuler\ForkJobRunner;
 
 use RuntimeException;
 use StephanSchuler\ForkJobRunner\Response\Response;
-use StephanSchuler\ForkJobRunner\Response\ThrowableResponse;
 use StephanSchuler\ForkJobRunner\Utility\PackageSerializer;
 use Traversable;
+use function assert;
 use function fclose;
 use function feof;
 use function fgets;
@@ -75,15 +75,12 @@ class Dispatcher
             throw new RuntimeException('Return channel unavailable', 1620514171);
         }
 
-        if (!$this->commandChannel) {
-            throw new RuntimeException('Command channel unavailable', 1620514174);
-        }
-        $put = fputs($this->commandChannel, PackageSerializer::toString($job), 1620514177);
+        $this->checkForRunningLoop();
+
+        assert(is_resource($this->commandChannel));
+        $put = fputs($this->commandChannel, PackageSerializer::toString($job));
         if ($put === false) {
-            yield new ThrowableResponse(
-                new RuntimeException('Command channel unavailable', 1620514181)
-            );
-            return;
+            throw new RuntimeException('Command channel unavailable', 1620514181);
         }
 
         while (true) {
@@ -105,12 +102,28 @@ class Dispatcher
                 }
             }
 
+            $this->checkForRunningLoop();
+
             if (feof($returnChannel)) {
                 break;
             }
         }
 
         fclose($returnChannel);
+    }
+
+    protected function checkForRunningLoop(): void
+    {
+        if (!$this->commandChannel || !is_resource($this->commandChannel)) {
+            throw new RuntimeException('Command channel unavailable', 1620514175);
+        }
+        if (!$this->loopProcess || !is_resource($this->loopProcess)) {
+            throw new RuntimeException('Loop process unavailable', 1620514177);
+        }
+        $status = proc_get_status($this->loopProcess);
+        if (!is_array($status) || !$status['running']) {
+            throw new RuntimeException('Loop process unavailable', 1620514178);
+        }
     }
 
     /** @param resource|null $process */
